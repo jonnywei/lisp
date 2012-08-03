a4778ee77accf8d0ecb45e2456b04571fdef8c43
Jonny Wei <weijianjun7100@gmail.com>
2012-08-03 21:28:50 +0800

    git add

--- hunt-the-wumpus.lisp
+++ hunt-the-wumpus.lisp
@@ -36,3 +36,62 @@
   (remove-if-not (lambda (x) 
 		   (eql (car x) node))
 		 edge-list))
+;; this function return all connected node
+(defun get-connected (node edge-list)
+  (let ((visited nil))
+    (labels ((traverse (node)
+	       (unless (member node visited)
+		 (push node visited)
+		 (mapc (lambda (edge)
+			 (traverse (cdr edge)))
+		       (direct-edges node edge-list)))))
+      (traverse node))
+    visited))
+
+
+;;find all islands
+(defun find-islands (nodes edge-list)
+  (let ((islands nil))
+    (labels ((find-island (nodes)
+	       (let* (( connected (get-connected (car nodes) edge-list))
+		      (unconnected (set-difference nodes connected)))
+		 (push connected islands)
+		 (when unconnected 
+		   (find-island unconnected)))))
+      (find-island nodes))
+    islands))
+	    
+
+;; connect with bridges
+
+(defun connect-with-bridges ( islands )
+  (when (cdr islands) ;  at least two islands ,if have one island, then all nodes be  connected
+    (append (edge-pair ( caar islands ) (caadr islands ))
+	    (connect-with-bridges (cdr islands )))))
+
+;; connectd all islands 
+(defun connect-all-islands (nodes edge-list)
+  (append (connect-with-bridges (find-islands  nodes edge-list)) edge-list))
+
+
+;;; make congistion city edges
+
+;; 
+(defun make-city-edges ()
+  (let* ((nodes (loop for i from 1 to *node-num* 
+		     collect i))
+	 (edge-list (connect-all-islands nodes (make-edge-list)))
+	 (cops (remove-if-not (lambda (x)
+				(zerop (random *cops-odds*))) ;1/*cop-odds* appear 0, so random ratio is 
+			      edge-list)))
+    (add-cops (edge-to-alist edge-list) cops)))
+
+
+(defun edges-to-alist (edge-list)
+  (mapcar (lambda (node1 )
+	    (cons node1 
+		  (mapcar (lambda (edge)
+			    (list (cdr edge)))
+			  (remove-duplicates (direct-edges node1 edge-list)
+					     :test #'equal))))
+	  (remove-duplicates (mapcar #'car edge-list))))
